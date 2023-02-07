84d26b2d4e5292516998d63018afdca6eb9dffe1
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2023-02-06 21:01:31 -0500

    build: adding kaleido theme colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "dk-primary": "#3942C1",
+      "dk-secondary": "#999999",
+      "dk-faded": "#EFF0FA",
+      "dk-background-gray": "#F2F2F2",
+      "dk-border-gray": "#CCCCCC",
+      "dk-primary-hover": "#21266F",
+      "dk-secondary-hover": "#565555",
+      "dk-connect-primary": "#EE34A8",
+      "dk-body-title": "#666666",
+    },
   },
   plugins: [],
 }
