ddfeb74566534f2e394d86065e62d2a4e4bbb752
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2023-02-08 18:42:15 -0500

    refactor: update layout to not use w-full

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,22 +30,22 @@ function App() {
       {/* site header */}
       <div className="flex flex-row py-3 border-b border-b-dk-border-gray">
         {avatar ?
-          <div className="flex w-1/4 mr-au my-auto text-4xl font-sans px-5">
+          <div className="flex ml-52 w-1/4 mr-au my-auto text-4xl font-sans px-5">
             Hi <img src={avatar} className="w-10 h-10" alt="logo" /> !
           </div> :
-          <div className="flex w-1/4 mr-au my-auto text-4xl font-sans px-5"/>
+          <div className="flex ml-52 w-1/4 mr-au my-auto text-4xl font-sans px-5"/>
         }
         <Link className="mx-auto w-14 h-14" to={'/'}>
           <img src={logo} alt="logo" />
         </Link>
-        <div className="w-1/4"></div>
+        <div className="mr-56 w-1/4"/>
       </div>
 
       {/* main content */}
       <div className="App flex flex-row h-full">
 
         {/* left sidebar */}
-        <div className="flex flex-col w-1/4 border-r p-5 border-r-dk-border-gray">
+        <div className="flex flex-col ml-52 w-1/4 border-r p-5 border-r-dk-border-gray">
           <Nav account={account} setAccount={setAccount} />
         </div>
 
@@ -55,7 +55,7 @@ function App() {
         </Routes>
 
         {/* right sidebar */}
-        <div className="flex flex-col w-1/4 border-l p-5 border-l-dk-border-gray">
+        <div className="flex flex-col mr-56 w-1/4 border-l p-5 border-l-dk-border-gray">
           <FollowingSideBar />
         </div>
       </div>
