bc9a46d5e5eb1be0784a9a63a914ae46b5963cc8
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2023-02-08 18:42:46 -0500

    fix: login should wait for response from api

--- frontend/src/components/nav.js
+++ frontend/src/components/nav.js
@@ -23,30 +23,28 @@ function Nav(props) {
             const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
             console.log('metamask accounts', accounts);
 
-            localStorage.setItem('account', accounts[0]);
-            props.setAccount(accounts[0]); // set account in parent component
-
             // get all users
-            const getUsers = await fetch('/api/users', {
-                method: 'GET',
-                headers: { 'Content-Type': 'application/json' }
-            });
-            console.log('getUsers', await getUsers.json());
+            // const getUsers = await fetch('/api/users', {
+            //     method: 'GET',
+            //     headers: { 'Content-Type': 'application/json' }
+            // });
+            // console.log('getUsers', await getUsers.json());
 
             // set public key to login endpoint
-            const loginResult = await fetch('/api/users/login', {
-                method: 'POST',
-                body: JSON.stringify({ publicKey: accounts[0] }),
-                headers: { 'Content-Type': 'application/json' }
-            });
 
-            console.log('login result', await loginResult.json());
+            try {
+                const loginResult = await fetch('/api/users/login', {
+                    method: 'POST',
+                    body: JSON.stringify({ publicKey: accounts[0] }),
+                    headers: { 'Content-Type': 'application/json' }
+                });
+                // console.log('login result', await loginResult.json());
 
-            // const balance = await ethereum.request({
-            //     method: 'eth_getBalance',
-            //     params: [accounts[0], 'latest'],
-            // });
-            // console.log('balance', balance);
+                localStorage.setItem('account', accounts[0]);
+                props.setAccount(accounts[0]); // set account in parent component
+            } catch (error) {
+                console.error('failed to login', error);
+            }
         }
         catch (error) {
             console.error(error);
@@ -88,7 +86,7 @@ function Nav(props) {
                 (<button
                     className="flex flex-row px-3 py-2 rounded border text-dk-faded bg-dk-primary hover:bg-dk-primary-hover active:bg-dk-primary cursor-pointer"
                     onClick={connectMetamask}>
-                    Connect Metamask <TbWallet className='flex my-auto ml-2' size={20}/>
+                    Login <TbWallet className='flex my-auto ml-2' size={20}/>
                 </button>
             )}
         </div>
