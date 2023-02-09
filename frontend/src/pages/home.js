496b63dee768ede84b2441c2fa3a8c2a110eaaec
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2023-02-08 22:33:28 -0500

    feat: populating home page with all posts

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import Post from '../components/post';
 
 function App() {
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     document.title = 'Nostr Home';
 
     fetch('/api/posts')
       .then(res => res.json())
-      .then(posts => console.log(posts.output));
+      .then(posts => {
+        console.log(posts.output)
+        setPosts(posts.output);
+      });
 
   }, []);
 
   return (
     <div className="w-full">
-      All the posts will be displayed here!
+      <div className="border-b border-b-dk-border-gray text-dk-secondary py-5">
+          See what what's happening on 🌎
+      </div>
+
+      {posts.map(post => <Post key={post.id} author={post.author} text={post.text} timestamp={post.timestamp}/>)}
     </div>
   );
 }
