00e7d76c8aa086d41bf568e33cd9e16753107714
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2023-02-09 14:41:45 -0500

    feat: populating followingSideBar

--- frontend/src/components/followingSideBar.js
+++ frontend/src/components/followingSideBar.js
@@ -1,42 +1,51 @@
+import { useEffect, useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AccountContext } from '../context/AccountContext';
+
+import { createAvatar } from '@dicebear/core';
+import { avataaars } from '@dicebear/collection';
 
 function FollowingSideBar() {
+  const accountContext = useContext(AccountContext);
+  const [following, setFollowing] = useState([]);
 
-    return (
-        <div className="flex flex-col space-y-1">
-            <Link
-                className="flex flex-row px-3 py-2 rounded border border-dk-border-gray text-dk-body-title bg-transparent hover:bg-dk-secondary-hover hover:text-dk-faded active:bg-dk-border-gray cursor-pointer"
-                to="/">
-                User 1
-            </Link>
-            <Link
-                className="flex flex-row px-3 py-2 rounded border border-dk-border-gray text-dk-body-title bg-transparent hover:bg-dk-secondary-hover hover:text-dk-faded active:bg-dk-border-gray cursor-pointer"
-                to="/feed">
-                User 2
-            </Link>
-            <Link
-                className="flex flex-row px-3 py-2 rounded border border-dk-border-gray text-dk-body-title bg-transparent hover:bg-dk-secondary-hover hover:text-dk-faded active:bg-dk-border-gray cursor-pointer"
-                to="/">
-                User 3
-            </Link>
-            <Link
-                className="flex flex-row px-3 py-2 rounded border border-dk-border-gray text-dk-body-title bg-transparent hover:bg-dk-secondary-hover hover:text-dk-faded active:bg-dk-border-gray cursor-pointer"
-                to="/feed">
-                User 4
-            </Link>
-            <Link
-                className="flex flex-row px-3 py-2 rounded border border-dk-border-gray text-dk-body-title bg-transparent hover:bg-dk-secondary-hover hover:text-dk-faded active:bg-dk-border-gray cursor-pointer"
-                to="/">
-                User 5
-            </Link>
-            <Link
-                className="flex flex-row px-3 py-2 rounded border border-dk-border-gray text-dk-body-title bg-transparent hover:bg-dk-secondary-hover hover:text-dk-faded active:bg-dk-border-gray cursor-pointer"
-                to="/feed">
-                User 6
-            </Link>
-        </div>
-    );
-  }
-  
-  export default FollowingSideBar;
-  
+  useEffect(() => {
+    // getting following for current user
+    fetch(`/api/users/following?publicKey=${accountContext.account}`)
+      .then(res => res.json())
+      .then(following => {
+        console.log('following', following.output);
+        if (following.error) {
+          console.error(following.error);
+          return;
+        }
+
+        if (following.output.length > 0) {
+          setFollowing(following.output);
+          return;
+        }
+      });
+  }, [accountContext.account]);
+
+  return (
+    <div className="flex flex-col space-y-1">
+      <div className="text-left font-light mt-0">
+        You are following :
+      </div>
+      {
+        following.map((followee) =>
+          <Link
+            className="flex flex-row px-3 py-2 text-sm rounded border border-dk-border-gray text-dk-body-title bg-transparent hover:bg-dk-secondary-hover hover:text-dk-faded active:bg-dk-border-gray cursor-pointer"
+            to={`/${followee.publicKey}`}>
+            <img className="w-8 h-8 rounded-full" src={createAvatar(avataaars, { seed: followee.publicKey }).toDataUriSync()} alt="avatar" />
+            <div className="my-auto ml-3">
+              {followee.publicKey.substring(0, 8)}...
+            </div>
+          </Link>
+        )
+      }
+    </div>
+  );
+}
+
+export default FollowingSideBar;
