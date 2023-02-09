fb856a6f38bbd892b47675f2ab65bfee4139c695
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2023-02-09 14:13:45 -0500

    fix: fixing feed to make sure we wait for fetch calls to finish before populating

--- frontend/src/pages/feed.js
+++ frontend/src/pages/feed.js
@@ -3,14 +3,14 @@ import PostEditor from "../components/postEditor";
 import Post from "../components/post";
 import { AccountContext } from "../context/AccountContext";
 
-function Feed() {
+function Feed(props) {
   const accountContext = useContext(AccountContext);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     document.title = "Nostr Feed";
 
-    if (!accountContext.account) {
+    if (!accountContext.account || posts.length > 0) {
       return;
     }
 
@@ -18,31 +18,41 @@ function Feed() {
     fetch(`/api/users/following?publicKey=${accountContext.account}`)
       .then(res => res.json())
       .then(following => {
-        console.log('following', following.output);
+        // console.log('following', following.output);
 
         if (following.error) {
           console.error(following.error);
           return;
         }
 
+        let postsArray = [];
+        let followeePromises = [];
         // for each following, get their posts
         for (const followee of following.output) {
-          fetch(`/api/users/${followee.publicKey}`)
-            .then(res => res.json())
-            .then(followeePosts => {
-              console.log(followeePosts);
-
-              if (followeePosts.error) {
-                console.error(followeePosts.error);
-                return;
-              }
-
-              setPosts([...posts, ...followeePosts.output]);
-            });
+          followeePromises.push(
+            new Promise(resolve => {
+              fetch(`/api/users/${followee.publicKey}`)
+                .then(res => res.json())
+                .then(followeePosts => {
+                  if (followeePosts.error) {
+                    console.error(followeePosts.error);
+                    return;
+                  }
+                  postsArray = [...postsArray, ...followeePosts.output]
+                  resolve();
+                })
+            })
+          );
         }
-      }
-      );
-  }, [accountContext.account]);
+
+        // wait for all followee posts to be fetched
+        Promise.all(followeePromises).then(() => {
+          // console.log('setting posts', postsArray)
+          setPosts(postsArray);
+        })
+      });
+
+  }, [accountContext.account, posts.length]);
 
   return (
     <div className="w-full px-5 overflow-y-auto">
@@ -63,7 +73,7 @@ function Feed() {
         }
       </div>
 
-      {posts.sort((a, b) => a.timestamp - b.timestamp).map(post => <Post key={post.id} author={post.author} text={post.text} timestamp={post.timestamp} />)}
+      {posts.sort((a, b) => b.timestamp - a.timestamp).map(post => <Post key={post.id} author={post.author} text={post.text} timestamp={post.timestamp} />)}
     </div>
   );
 }
